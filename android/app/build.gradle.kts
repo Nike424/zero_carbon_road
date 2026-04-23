bdb8147d12fbfa8e5c0cdeb66a33208eddbaa8ba
Nike424 <1473791367@qq.com>
2026-04-23 23:46:06 +0800

    Fix formatting in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,11 +26,12 @@ android {
         versionName = flutter.versionName
     }
 
-   buildTypes {
-    release {
-        signingConfig = signingConfigs.getByName("debug")
-        isMinifyEnabled = false   // 关闭代码混淆
-        isShrinkResources = false // 关闭资源压缩
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
     }
 }
 
